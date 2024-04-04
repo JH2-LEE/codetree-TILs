758d223679d5b6e6058a8f2df5305eda2087ac22
Jihui Lee <jihui@kaist.ac.kr>
2024-04-05 00:05:04 +0900

    Title: 코드트리 메신저, Time: 143ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240405/코드트리 메신저/codetree-messenger.py
+++ 240405/코드트리 메신저/codetree-messenger.py
@@ -30,8 +30,8 @@ def toggle_noti(c):
         else:
             message[par][0] += 1
         cur = par
-        # if noti[cur]:
-        #     break
+        if noti[cur]:
+            break
     for aa, t in enumerate(message[c]):
         if aa == 0 or t == 0:
             continue
@@ -44,8 +44,8 @@ def toggle_noti(c):
             else:
                 message[par][k] += t
             cur = par
-            # if noti[cur]:
-            #     break
+            if noti[cur]:
+                break
     noti[c] = not noti[c]  # 알람 변경
 
 def change_power(c, power):
